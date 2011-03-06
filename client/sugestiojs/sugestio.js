408fe796daf2bf510dfb7d02cd210c8af4309e87
Michiel Gyssels <michielgyssels@gmail.com>
2011-03-06 17:19:52 +0100

    submit functions work

--- client/sugestiojs/sugestio.js
+++ client/sugestiojs/sugestio.js
@@ -1,11 +1,181 @@
 function scriptsLoaded() {
     (function () {
         var remoteURL = "http://api.sugestio.com/michiel",
-            userid = -1,
+            userid = null,
             i = 0,
-            submitTypes = "consumptions items users".split(" "),
-            
-            userRecommendations = "recommendations"
+            getSubmitData = function(data) {
+                var result = {},
+                    i = 0;
+                for (dataKey in data) {
+                    var dataValue = data[dataKey],
+                        endsWithEl = new RegExp("El$"),
+                        elements = 0;
+                    if(endsWithEl.test(dataKey)){
+                        //get the current value of that data field
+                        var newKey = dataKey.replace(endsWithEl, "");
+                        if (typeof dataValue === "string") {
+                            elements = Sizzle(dataValue);
+                        } else {
+                            elements = dataValue;
+                        }
+                        switch(elements.length) {
+                        case 1:
+                            result[newKey] = elements[0].value;
+                            break;
+                        default:
+                            //multiple elements are only allowed for radioboxes
+                            try{
+                                for (i=0; i<elements.length; i++) {
+                                    if (elements[i].nodeName === "INPUT" && elements[i].type === "radio") {
+                                        if(elements[i].checked){
+                                            result[newKey] = elements[i].value;
+                                            break;
+                                        }
+                                    }
+                                    else throw "only radioboxes allowed";
+                                }
+                            }
+                            catch(err){
+                                console.log(err);
+                            }
+                        }
+                    } else{
+                        result[dataKey] = dataValue;
+                    }
+                }
+                return result;
+            }
+            sugestio = {
+                user: {
+                    url: "/sites/sandbox/users",
+                    checkData: function(data){
+                        if(typeof data.userid !== "number" && data.userid !== "string"){
+                            console.log('User not given');
+                            return false;
+                        }
+                        return true;
+                    },
+                    getSubmitData: function(inputData){
+                        var result = getSubmitData(inputData);
+                        if(typeof result.userid === "undefined"){
+                            result.userid = userid;
+                        }
+                        return result;
+                    }
+                },
+                item: {
+                    url: "/sites/sandbox/items",
+                    checkData: function(data){
+                        if(typeof data.itemid !== "number" && data.itemid !== "string"){
+                            console.log('User not given');
+                            return false;
+                        }
+                        return true;
+                    },
+                    getSubmitData: getSubmitData
+                },
+                consumptions: {
+                    url: "/sites/sandbox/consumptions",
+                    checkData: function(data){
+                        if(typeof data.itemid !== "number" && data.itemid !== "string"){
+                            console.log('item not given');
+                            return false;
+                        }
+                        if(typeof data.userid !== "number" && data.userid !== "string"){
+                            console.log('user not given');
+                            return false;
+                        }
+                        return true;
+                    },
+                    getSubmitData: function(inputData){
+                        var result = getSubmitData(inputData);
+                        if(typeof result.userid === "undefined"){
+                            result.userid = userid;
+                        }
+                        return result;
+                    }
+                },
+                url : "/sites/sandbox/"
+            },
+            functionDescrs = [{
+                type: "submit",
+                name: "view",
+                binding: sugestio.consumptions,
+                checkData: sugestio.consumptions.checkData,
+                getSubmitData: sugestio.consumptions.getSubmitData
+            }, {
+                type: "submit",
+                name: "rating",
+                binding: sugestio.consumptions,
+                checkData: sugestio.consumptions.checkData,
+                getSubmitData: function(inputData){
+                    var result = sugestio.consumptions.getSubmitData(inputData);
+                    if (typeof result.max === "number" && typeof result.min === "number" && (typeof result.rating === "number" || typeof result.rating === "string")) {
+                        result.detail = "STAR:" + result.max + ":" + result.min + ":" + result.rating;
+                        delete result.max;
+                        delete result.min;
+                        delete result.rating;
+                    }
+                    return result;
+                }
+            }, {
+                type: "submit",
+                name: "wishlist",
+                binding: sugestio.consumptions,
+                checkData: sugestio.consumptions.checkData,
+                getSubmitData: sugestio.consumptions.getSubmitData
+            }, {
+                type: "submit",
+                name: "basket",
+                binding: sugestio.consumptions,
+                checkData: sugestio.consumptions.checkData,
+                getSubmitData: sugestio.consumptions.getSubmitData
+            }, {
+                type: "submit",
+                name: "purchase",
+                binding: sugestio.consumptions,
+                checkData: sugestio.consumptions.checkData,
+                getSubmitData: sugestio.consumptions.getSubmitData
+            }, {
+                type: "submit",
+                name: "collection",
+                binding: sugestio.consumptions,
+                checkData: sugestio.consumptions.checkData,
+                getSubmitData: sugestio.consumptions.getSubmitData
+            }, {
+                type: "submit",
+                name: "checkin",
+                binding: sugestio.consumptions,
+                checkData: sugestio.consumptions.checkData,
+                getSubmitData: sugestio.consumptions.getSubmitData
+            }, {
+                type: "submit",
+                name: "meta",
+                binding: sugestio.user,
+                checkData: sugestio.user.checkData,
+                getSubmitData: sugestio.user.getSubmitData
+            }, {
+                type: "submit",
+                name: "meta",
+                binding: sugestio.item,
+                checkData: sugestio.item.checkData,
+                getSubmitData: sugestio.item.getSubmitData
+            }, {
+                type: "recommendations",
+                name: "similar",
+                binding: sugestio.item,
+                checkData: sugestio.user.checkData
+            }, {
+                type: "recommendations",
+                name: "similar",
+                binding: sugestio.user,
+                checkData: sugestio.user.checkData
+            }, {
+                type: "recommendations",
+                name: "recommendations",
+                binding: sugestio.user,
+                checkData: sugestio.user.checkData
+            }];
             socket = new easyXDM.Socket({
                 local: "index.html",
                 remote: remoteURL + "/index.html",
@@ -23,8 +193,8 @@ function scriptsLoaded() {
                     }
                 }
             });
-        window.sugestio = window.sugestio || {};
-        sugestio.user = function (a, b) {
+        window.sugestio = sugestio;
+        sugestio.user.login = function (a, b) {
             if (typeof a === "function") {
                 if (typeof b === "undefined") {
                     userid = a.apply(this, []);
@@ -35,111 +205,47 @@ function scriptsLoaded() {
                 userid = a;
             }
         };
-        
-        sugestio.recommendations = function (func, scope) {
-            if (userid !== -1) {
-                remoteCall('get', ['/sites/sandbox/users/' + userid + '/recommendations.json'], {
-                    success: function (resp) {
-                        if (typeof func === "function") {
-							if(typeof scope !== "undefined"){
-                            	func.apply(this, [resp]);
-							} else {
-								func.apply(scope, [resp]);
-							}
-                        }
-                    },
-                    error: function (resp) {
-                        console.log("ERROR: " + resp);
-                    }
-                });
-            }
-        };
-        sugestio.similarItems = function (itemid,func, scope) {
-            remoteCall('get', ['/sites/sandbox/items/' + itemid + '/similar.json'], {
-                success: function (resp) {
-                    if (typeof func === "function") {
-                        if(typeof scope !== "undefined"){
-                            func.apply(this, [resp]);
-                        } else {
-                            func.apply(scope, [resp]);
-                        }
-                    }
-                },
-                error: function (resp) {
-                    console.log("ERROR: " + resp);
+		
+        var registerFunction = function (funcDescr) {
+            function submit(inputData) {
+                if(funcDescr.binding === sugestio.consumptions){
+                    inputData.type = funcDescr.name.toUpperCase();
                 }
-            });
-        };
-		function getData(data) {
-			var result = {},
-			    i = 0;
-			for (dataKey in data) {
-				var dataValue = data[dataKey],
-				    endsWithEl = new RegExp("El$"),
-				    elements = 0;
-				if(endsWithEl.test(dataKey)){
-					//get the current value of that data field
-					var newKey = dataKey.replace(endsWithEl, "");
-					if (typeof dataValue === "string") {
-						elements = Sizzle(dataValue);
-					} else {
-						elements = dataValue;
-					}
-					switch(elements.length) {
-					case 1:
-						result[newKey] = elements[0].value;
-						break;
-					default:
-						//multiple elements are only allowed for radioboxes
-						try{
-							for (i=0; i<elements.length; i++) {
-								if (elements[i].nodeName === "INPUT" && elements[i].type === "radio") {
-									if(elements[i].checked){
-										result[newKey] = elements[i].value;
-										break;
-									}
-								}
-								else throw "only radioboxes allowed";
-							}
-						}
-						catch(err){
-							console.log(err);
-						}
-					}
-				} else{
-					result[dataKey] = dataValue;
-				}
-			}
-			//if the type is a rating: format the rating to the detail
-			if (result.type === "RATING" && typeof result.detail === "undefined") {
-				if (typeof result.max === "number" && typeof result.min === "number" && (typeof result.rating === "number" || typeof result.rating === "string")) {
-					result.detail = "STAR:" + result.max + ":" + result.min + ":" + result.rating;
-					delete result.max;
-					delete result.min;
-					delete result.rating;
-				}
-			}
-			return result;
-		}
-		function call(submitType,data) {
-			var data2 = getData(data);
-			console.log(data2);
-			if(submitType === "consumptions"){
-			    data2.userid = userid;
-			}
-			remoteCall('post', ['/sites/sandbox/' + submitType, data2], {
-				success: function (resp) {
-					console.log(resp);
-				},
-				error: function (resp) {
-					console.log("ERROR with /sites/sandbox/" + urlSuffix + ": " + resp);
-				}
-			});
-		}
-        for (i = submits.length; i--;) {
-            (function (submitType) {
-                sugestio[submitType] = function (data, triggerEl) {
-                    if (userid !== -1 && typeof triggerEl !== "undefined") {
+                var submitData = funcDescr.getSubmitData(inputData);
+                console.log(submitData);
+                if(typeof funcDescr.checkData !== "function" || funcDescr.checkData(submitData)){
+                    remoteCall('post', [funcDescr.binding.url, submitData], {
+                        success: function (resp) {
+                            console.log(resp);
+                        },
+                        error: function (resp) {
+                            console.log("ERROR with " + funcDescr.binding.url +funcDescr.name + ": " + resp);
+                        }
+                    });
+                }
+            }
+            switch(funcDescr.type){
+            case 'recommendations':
+                funcDescr.binding[funcDescr.name] = function (func, scope) {
+                    remoteCall('get', [funcDescr.binding.url +funcDescr.name + '.json'], {
+                        success: function (resp) {
+                            if (typeof func === "function") {
+                                if(typeof scope !== "undefined"){
+                                    func.apply(this, [resp]);
+                                } else {
+                                    func.apply(scope, [resp]);
+                                }
+                            }
+                        },
+                        error: function (resp) {
+                            console.log("ERROR: " + resp);
+                        }
+                    });
+                };
+                break;
+            case 'submit':
+                funcDescr.binding[funcDescr.name] = function (inputData, triggerEl) {
+                    if (typeof triggerEl !== "undefined") {
                         var triggers = [];
                         if (typeof triggerEl === "string") {
                             triggers = Sizzle(triggerEl);
@@ -150,31 +256,38 @@ function scriptsLoaded() {
                             switch (triggers[i].nodeName) {
                             case 'BUTTON':
                                 triggers[i].addEventListener('click', function () {
-                                    call(submitType,data);
+                                    submit(inputData);
                                 }, false);
                                 break;
                             case 'INPUT':
                                 triggers[i].addEventListener('change', function () {
                                     if (this.checked) {
-                                        call(submitType,data);
+                                        submit(inputData);
                                     }
                                 }, false);
                                 break;
                             case 'FORM':
-								triggers[i].addEventListener('submit', function () {
-									call(submitType,data);
-								}, false);
-								break;
+                                triggers[i].addEventListener('submit', function () {
+                                    submit(inputData);
+                                }, false);
+                                break;
                             }
                         }
                     } else {
-                        call(submitType,data);
+                        submit(inputData);
                     }
                 };
-            })(submits[i]);
-        }
+                break;
+            }
+        };
+        (function () {
+            for (i = 0; i<functionDescrs.length; i++) {
+                var funcDescr = functionDescrs[i];
+                registerFunction(funcDescr);
+            }
+        })();
         sugestio.ratingWidget = function (data) {
-            if (typeof data === "object" && data.contentEl && data.type && data.itemid) {
+            if (typeof data === "object" && data.contentEl && data.itemid) {
                 var elements = Sizzle(data.contentEl),
                     i = 0;
                 for (i = 0; i < elements.length; i++) {
@@ -235,14 +348,13 @@ function scriptsLoaded() {
 						inputEl.setAttribute('value', i);
 						inputEls.push(inputEl);
 						if (typeof data.formEl === "undefined") {
-							sugestio.consumptions({type: 'RATING', 'itemid': itemid, detail: 'STAR:'+data.max+':'+data.min+':' + inputEl.value}, inputEl);
+							sugestio.consumptions.rating({'itemid': itemid, detail: 'STAR:'+data.max+':'+data.min+':' + inputEl.value}, inputEl);
 						}
 						label.appendChild(inputEl);
 						span.appendChild(label);
 					}
 					if (typeof data.formEl !== "undefined" && data.formEl) {
-						sugestio.consumptions({
-							type: 'RATING', 
+						sugestio.consumptions.rating({
 							'itemid': itemid,
 							ratingEl: inputEls,
 							max: data.max,
